ad80cecbad6e4a5528688388e50af5b89d3d6ca5
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-04-26 18:17:53 -0300

    Series e inicio do pandas

--- Pandas/series.py
+++ Pandas/series.py
@@ -1,21 +1,59 @@
-"""Series são basicamente arrays unidimensionais podendo possuir vários tipos de dados"""
-
 import pandas as pd
 
-serie = pd.Series([1, True, "texto", 3.14], [0, 1, 2, 3])
-int_serie = pd.Series([1, 2, 3,4 ,5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9])
-char_serie = pd.Series(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-float_serie = pd.Series([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-bool_serie = pd.Series([True, False], [1, 0])
+"""As series são basicamente arrays, mas com pequenos adicionais que permitem certas interações com outras estruturas"""
 
-# Métodos sobre os atrributos da serie
+# Crinado uma serie
 
-# Vendo os tipos de dados de uma  serie
-dados = int_serie.dtype # Ou dtypes, mas este é para Data Frames
+# Jeito direto
+serie = pd.Series(values = ("a", "b", "c", "d"), index = (1, 2, 3, 4))
 
-# Vendo os indexes
-index = char_serie.index
+# A partir de uma lista ou tupla
+lista = ["a", "b", "c", "d"]
+tupla = (1, 2, 3)
 
-# Vendo o vaor de cada item do array
-valores = serie.values
-print(valores)
+serie_list = pd.Series(lista)
+serie_tupla = pd.Series(tupla)
+
+# Também é possível com sets, mas a ordem pode acabar variando a cada execução
+
+# É possível fazer o mesmo com um dicionário, mas nesse caso as chaves viram os indices
+
+dict = {
+    1 : "a",
+    2 : "b",
+    3 : "c",
+    4 : "d"
+}
+
+serie_dict = pd.Series(dict)
+
+# Repetindo um valor para todas as colunas
+
+colunas = pd.Series(values = 7, index = [1, 2, 3, 4])
+
+# Series permitem operações de fatiamento, filtro, operações matemáticas
+
+# Os métodos próprios de uma serie são
+
+# Para acesso
+serie.get(2, 0) # Retorna 0 se não encontrar
+#serie["2"] Retorna erro se não encontrar
+
+# Em relação aos nulos
+serie.isnull() # Retorna True para todos os nulos que encontrar
+serie.notnull() # Retona False pata todos os nulos que encontrar
+serie.fillna(0) # Substitui os nullos pelo valor passado. Pode ser usado com methods como ffill(substitui pelo anterior), bfill(substitui pelo posterior), etc
+
+# Estatístisca
+serie.mean()
+serie.sum()
+serie.describe() # Devolve um resumo da serie, quantos elementos, maior elemento, média, etc
+serie.count()
+# etc
+
+# Atrtibutos utéis
+serie.values()
+serie.index()
+dtype = serie.dtype
+shape = serie.shape
+name = serie.name
